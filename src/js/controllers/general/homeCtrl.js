9236c93c74d9e062ebdaaa0bab9a3504045c032c
austin ezell <austin.r.ezell@gmail.com>
2015-12-14 13:35:44 -0800

    adds small stylistic change to home view and modularizes faq variable into own file

--- src/js/controllers/general/homeCtrl.js
+++ src/js/controllers/general/homeCtrl.js
@@ -1,29 +1,11 @@
 'use strict';
 
 import app from '../../app.js';
+import faqs from '../../misc/faq.js';
 
 app.controller('homeCtrl', ['$scope', 'Pitch', function($scope, Pitch) {
 
-  $scope.faqs = [{
-    question: "Does your amazing service cost anything?",
-    answer: "Not a damn thing. If you want to donate, we do \
-    accept payment in the form of antique photos/paintings \
-    of lighthouses. (If you really want to donate, but can't \
-    afford a lighthouse we'll take a windmill)."
-  }, {
-    question: "Do you handle payment if people working on a project \
-    agree to some sort of compensation?",
-    answer: "No. Venmo will! Or PayPal. Or wire transfers from a bank. \
-    Or cash. Or anything that isn't us. We don't handle that."
-  }, {
-    question: "How does it work?",
-    answer: "Technology, man. Technology."
-  }, {
-    question: "How do I get started?",
-    answer: "Elementary, Watson! Click the dropdown menu, then go to \"Login\" \
-    create an account and you will be given the oppurtunity to pitch an idea \
-    and/or work with others on ideas!"
-  }]
+  $scope.faqs = faqs;
 
   Pitch.getAll().then(res => {
     $scope.pitches = res.data.reverse()
---
+++ src/js/misc/faq.js
@@ -0,0 +1,24 @@
+"use strict";
+
+const FAQs = [{
+  question: "Does your amazing service cost anything?",
+  answer: "Not a damn thing. If you want to donate, we do \
+  accept payment in the form of antique photos/paintings \
+  of lighthouses. (If you really want to donate, but can't \
+  afford a lighthouse we'll take a windmill)."
+}, {
+  question: "Do you handle payment if people working on a project \
+  agree to some sort of compensation?",
+  answer: "No. Venmo will! Or PayPal. Or wire transfers from a bank. \
+  Or cash. Or anything that isn't us. We don't handle that."
+}, {
+  question: "How does it work?",
+  answer: "Technology, man. Technology."
+}, {
+  question: "How do I get started?",
+  answer: "Elementary, Watson! Click the dropdown menu, then go to \"Login\" \
+  create an account and you will be given the oppurtunity to pitch an idea \
+  and/or work with others on ideas!"
+}]
+
+export default FAQs
