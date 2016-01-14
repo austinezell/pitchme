9716a28fac95e297d0b2c959d61087d394a519b2
austin ezell <austin.r.ezell@gmail.com>
2016-01-14 15:06:00 -0800

    fixes mongolab variable name.

--- config/constants.js
+++ config/constants.js
@@ -1,5 +1,5 @@
 module.exports = {
   JWT_SECRET: (process.env.JWT_SECRET || "Secret"),
-  MONGO_URL: (process.env.MONGO_URL || process.env.MONGO_URI || "mongodb://localhost/pitchMe"),
+  MONGO_URL: (process.env.MONGO_URL || process.env.MONGOLAB_URI || "mongodb://localhost/pitchMe"),
   PORT: (process.env.PORT || 3030)
 }
