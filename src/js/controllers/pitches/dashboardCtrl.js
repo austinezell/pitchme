1301755e6ad6040de142314a1c52a32ed9968fbf
austin ezell <austin.r.ezell@gmail.com>
2016-02-04 18:37:43 -0800

    readjusts margin and creates templates for issues and developers tab

--- src/js/controllers/pitches/dashboardCtrl.js
+++ src/js/controllers/pitches/dashboardCtrl.js
@@ -3,7 +3,14 @@ import app from "../../app.js";
 app.controller('dashboardCtrl', ["$scope", "Pitch", "User", "$stateParams", function($scope, Pitch, User, $stateParams){
   Pitch.getDetails($stateParams.id)
   .success(data => {
-    $scope.pitch = data;
+    generateDates(data)
     console.log(data);
   })
+
+  let generateDates = (pitch) =>{
+    pitch.datePitched = new Date(pitch.datePitched);
+    if (pitch.dateCompleted) pitch.dateCompleted = new Date(pitch.dateCompleted);
+    $scope.pitch = pitch;
+  }
+
 }])
