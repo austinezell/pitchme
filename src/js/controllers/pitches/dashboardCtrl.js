4e45d1a051ed05b182446071a7c8d1f9875aa6f6
austin ezell <austin.r.ezell@gmail.com>
2016-02-08 21:43:33 -0800

    I pushed broken code :(

--- src/js/controllers/pitches/dashboardCtrl.js
+++ src/js/controllers/pitches/dashboardCtrl.js
@@ -1,18 +1,21 @@
 import app from "../../app.js";
 
-app.controller('dashboardCtrl', ["$scope", "Pitch", "User", "$state", "$stateParams", function($scope, Pitch, User, $state, $stateParams){
+app.controller('dashboardCtrl', ["$scope", "Pitch", "User", "$state", "$stateParams", "$rootScope", function($scope, Pitch, User, $state, $stateParams, $rootScope){
   Pitch.getDetails($stateParams.id)
   .success(data => {
-    formatData(data);
+    generateDates(data);
     console.log(data);
   })
 
   $scope.currentLocation = {};
 
-  let formatData = (pitch) =>{
+  let generateDates = (pitch) =>{
     pitch.datePitched = new Date(pitch.datePitched);
     if (pitch.dateCompleted) pitch.dateCompleted = new Date(pitch.dateCompleted);
     $scope.pitch = pitch;
+    pitch.administrators.forEach(admin => {
+      if (admin._id === $rootScope.currentUser._id) $scope.isAdmin = true;
+    });
     $scope.issues = $scope.pitch.issues.reverse().filter( (issue)=> !issue.isResolved).slice(0, 3);
     $scope.pitch.openIssues = $scope.pitch.issues.filter( (issue)=> !issue.isResolved);
   }
